d896ed455ba10aa8f81ead4f309a8f0f79b84345
Ahmed Soliman <ahmed.gh.soliman@gmail.com>
2025-12-11 00:03:04 +0100

    Replace database para with DB_URL environment variable

--- note_api/__init__.py
+++ note_api/__init__.py
@@ -9,20 +9,10 @@ from sqlalchemy.exc import SQLAlchemyError
 db = SQLAlchemy()
 
 
-def create_app(config_object="config.DevelopmentConfig"):
+def create_app():
     app = Flask(__name__)
 
-    # Database para
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_NAME = os.getenv("DB_NAME")
-
-    db_url = os.getenv(
-        "DB_URL",
-        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}",
-    )
-
+    db_url = os.getenv("DB_URL")
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
