2e3ed18115ee0b99702868140f2835a7f60b1c9a
Ahmed Soliman <ahmed.gh.soliman@gmail.com>
2025-12-10 01:28:15 +0100

    autoincrement for id column.

--- note_api/models.py
+++ note_api/models.py
@@ -3,7 +3,7 @@ from . import db
 
 class Note(db.Model):
     __tablename__ = "notes"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
 
